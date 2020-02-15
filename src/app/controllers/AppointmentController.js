879c90f2d85a5841980cdb98f9151ab6af763a88
dougglas1 <dougglas1994@hotmail.com>
2020-02-14 22:06:09 -0300

    Adicionar Appointment e Schedule, além do MongoDB

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,38 @@
 import * as Yup from 'yup';
+import { startOfHour, parseISO, isBefore } from 'date-fns';
 import User from '../models/User';
+import File from '../models/File';
 import Appointment from '../models/Appointment';
 
 class AppointmentController {
+  async index(req, res) {
+    const { page = 1 } = req.query;
+
+    const appointments = await Appointment.findAll({
+      where: { user_id: req.userId, canceled_at: null },
+      order: ['date'],
+      limit: 20,
+      offset: (page - 1) * 20, // Mostrar 20 por página
+      attributes: ['id', 'date'],
+      include: [
+        {
+          model: User,
+          as: 'provider',
+          attributes: ['id', 'name'],
+          include: [
+            {
+              model: File,
+              as: 'avatar',
+              attributes: ['id', 'path', 'url'],
+            },
+          ],
+        },
+      ],
+    });
+
+    return res.json(appointments);
+  }
+
   async store(req, res) {
     const schema = Yup.object().shape({
       provider_id: Yup.number().required(),
@@ -23,7 +53,28 @@ class AppointmentController {
     if (!checkIsProvider) {
       return res
         .status(401)
-        .json({ error: ' Você somente pode criar compromisso como provedor' });
+        .json({ error: 'Você somente pode criar compromisso como provedor' });
+    }
+
+    // parseISO transforma a string date em objeto date do JS
+    // startOfHour arredondamento  (19:30) > (19)
+    const hourStart = startOfHour(parseISO(date));
+
+    // Verifica se está antes da data atual
+    if (isBefore(hourStart, new Date())) {
+      return res.status(400).json({ error: 'Data não permitida' });
+    }
+
+    const checkAvalilability = await Appointment.findOne({
+      where: {
+        provider_id,
+        canceled_at: null,
+        date: hourStart,
+      },
+    });
+
+    if (checkAvalilability) {
+      return res.status(400).json({ error: 'Data não acessível' });
     }
 
     const appointment = await Appointment.create({
